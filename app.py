69ee2af25b518f9ed9e0a3ff989d9edd1aa151de
antonie <53366054+antonie-riziki@users.noreply.github.com>
2025-07-20 09:36:25 +0000

    changes to the app.py main page

--- app.py
+++ app.py
@@ -11,7 +11,18 @@ from upload_file_rag import get_qa_chain, query_system
 
 
 
+page_config = {"page_title":"GDG IO Nairobi 2025", "page_icon":":desktop computer:", "layout":"centered"}
+st.set_page_config(**page_config)
 
+
+with st.sidebar:
+	selected = option_menu(
+		menu_title = 'Menu',
+		options = ['Home', 'Chatbot', 'Document Chat', 'Image Gen'],
+		icons = ['speedometer', 'chat-dots', 'currency-bitcoin', 'activity'],
+		menu_icon = 'cast',
+		default_index = 0
+		)
 
 
 st.image('https://linktr.ee/og/image/gdgnairobi.jpg', width=700)
@@ -20,55 +31,59 @@ st.image('https://linktr.ee/og/image/gdgnairobi.jpg', width=700)
 # col1, col2 = st.columns(2)
 
 # with col1:
-def reset_conversation():
-  st.session_state.conversation = None
-  st.session_state.chat_history = None
+# def reset_conversation():
+#   st.session_state.conversation = None
+#   st.session_state.chat_history = None
 
 
-with st.sidebar:
-    pass
-    # if st.button(label="", icon=":material/quick_reference_all:", on_click=reset_conversation):
-    #     with st.spinner("Refreshing chat... Please wait."):
-    #         st.success("Chat refreshed successfully!")
-
-uploaded_files = st.file_uploader('Upload a File', accept_multiple_files=True)
-
-if uploaded_files:
-    for uploaded_file in uploaded_files:
-
-        suffix = os.path.splitext(uploaded_file.name)[1]
-        with tempfile.NamedTemporaryFile(delete=False, suffix=suffix) as temp_file:
-            temp_file.write(uploaded_file.getbuffer())
-            temp_path = temp_file.name
-
-        # Initialize QA chain from saved file
-        qa_chain = get_qa_chain(temp_path)
-
-        # Initialize session state for chat history
-        if "messages" not in st.session_state:
-            st.session_state.messages = [{"role": "assistant", "content": "How may I help you?"}]
-
-        # Display chat history
-        for message in st.session_state.messages:
-
-            with st.chat_message(message["role"]):
-                st.markdown(message["content"])
+# with st.sidebar:
+#     pass
+#     # if st.button(label="", icon=":material/quick_reference_all:", on_click=reset_conversation):
+#     #     with st.spinner("Refreshing chat... Please wait."):
+#     #         st.success("Chat refreshed successfully!")
 
 
-        if prompt := st.chat_input("How may I help?", key='RAG chat'):
-            # Append user message
-            st.session_state.messages.append({"role": "user", "content": prompt})
-            with st.chat_message("user"):
-                st.markdown(prompt)
+if select=="Home":
 
-            # Generate AI response
-            chat_output = query_system(prompt, qa_chain)
-            
-            # Append AI response
-            with st.chat_message("assistant"):
-                st.markdown(chat_output)
 
-            st.session_state.messages.append({"role": "assistant", "content": chat_output})
+    uploaded_files = st.file_uploader('Upload a File (PDF/CSV)', accept_multiple_files=True)
+
+    if uploaded_files:
+        for uploaded_file in uploaded_files:
+
+            suffix = os.path.splitext(uploaded_file.name)[1]
+            with tempfile.NamedTemporaryFile(delete=False, suffix=suffix) as temp_file:
+                temp_file.write(uploaded_file.getbuffer())
+                temp_path = temp_file.name
+
+            # Initialize QA chain from saved file
+            qa_chain = get_qa_chain(temp_path)
+
+            # Initialize session state for chat history
+            if "messages" not in st.session_state:
+                st.session_state.messages = [{"role": "assistant", "content": "How may I help you?"}]
+
+            # Display chat history
+            for message in st.session_state.messages:
+
+                with st.chat_message(message["role"]):
+                    st.markdown(message["content"])
+
+
+            if prompt := st.chat_input("How may I help?", key='RAG chat'):
+                # Append user message
+                st.session_state.messages.append({"role": "user", "content": prompt})
+                with st.chat_message("user"):
+                    st.markdown(prompt)
+
+                # Generate AI response
+                chat_output = query_system(prompt, qa_chain)
+                
+                # Append AI response
+                with st.chat_message("assistant"):
+                    st.markdown(chat_output)
+
+                st.session_state.messages.append({"role": "assistant", "content": chat_output})
 
 
 
